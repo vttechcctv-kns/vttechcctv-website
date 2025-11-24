9933758e8fca8cbaf1e9d4583d4f513b416ec8b6
vttechcctv-kns <vttechcctv@gmail.com>
2025-11-24 21:24:16 +0700

    Update products1.js

--- products1.js
+++ products1.js
@@ -5,14 +5,7 @@ window.VT_PRODUCTS = {
     "id": "p1",
     "name": "Camera IP 2Mp DS-2CD1T27GO-LUF",
     "price": 1190000,
-    "description": "– Cảm biến ảnh: 1/2.8″ CMOS 2.0 megapixel."
-                    "description": "– Độ nhạy sáng 0.001 Lux, H.265+."
-                    "description": "– Độ phân giải 1920 x 1080/ 25fps."
-                    "description": "– Ống kính 4/6mm @F1.0"
-                    "description": "– Hỗ trợ đèn sáng trắng, tối đa 50m"
-                    "description": "– 3D DNR, DWDR, BLC, HLC"
-                     "description":"– Hỗ trợ thẻ nhớ 256GB; Tích hợp Mic"
-                    "description": "– Tiêu chuẩn IP67",
+    "description": "– Cảm biến ảnh: 1/2.8″ CMOS 2.0 megapixel.",
     "image": "Camera/DS-2CD1T27GO-LUF.png",
     "sku": "DS-2CD1T27GO-LUF",
     "warranty": "12 tháng",
